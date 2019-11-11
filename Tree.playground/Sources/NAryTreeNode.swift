61114f6c30d0a744a73de373f8b1ca9249fea574
Dilgir Siddiqui <dsiddiqui@apple.com>
2019-11-10 19:05:58 -0800

    N-ary tree implementation and traversal

--- Tree.playground/Sources/NAryTreeNode.swift
+++ Tree.playground/Sources/NAryTreeNode.swift
@@ -3,24 +3,78 @@ import Foundation
 public class NAryTreeNode<T> {
     
     public var data: T
-    public var children: [NAryTreeNode?]?
+    public var children = [NAryTreeNode<T>]()
+    public weak var parent: NAryTreeNode<T>?
     
-    public init(data: T, children: [NAryTreeNode?]? = nil) {
+    public init(data: T) {
         self.data = data
-        self.children = children
     }
 }
 
-//extension BSTNode: Equatable where T: Equatable {
-//        
-//    public static func ==(lhs: BSTNode<T>, rhs: BSTNode<T>) -> Bool {
-//        return lhs.data == rhs.data
-//    }
-//}
-//
-//extension BSTNode: Comparable where T: Comparable {
-//
-//    public static func <(lhs: BSTNode<T>, rhs: BSTNode<T>) -> Bool {
-//        return lhs.data < rhs.data
-//    }
-//}
+// ==================================================================
+// Tree - Add Nodes
+// ==================================================================
+
+extension NAryTreeNode {
+    
+    public func addChild(_ node: NAryTreeNode<T>) {
+        children.append(node)
+        node.parent = self
+    }
+}
+
+extension NAryTreeNode: CustomStringConvertible {
+    
+    public var description: String {
+        var s = "\(data)"
+        if !children.isEmpty {
+            s += " {" + children.map { $0.description }.joined(separator: ", ") + "}"
+        }
+        return s
+    }
+}
+
+// ==================================================================
+// Tree - Search
+// ==================================================================
+extension NAryTreeNode where T: Equatable {
+    public func search(_ value: T) -> NAryTreeNode<T>? {
+        if value == self.data {
+            return self
+        }
+        for child in children {
+            if let found = child.search(value) {
+                return found
+            }
+        }
+        return nil
+    }
+}
+
+// ==================================================================
+// Tree - Traversal
+// ==================================================================
+
+extension NAryTreeNode {
+    
+    public func preOrderTraversal(withRootNode node: NAryTreeNode<T>?) {
+        guard let node = node else {
+            return
+        }
+        print("\(node.data)", terminator: " --> ")
+        for child in node.children {
+            preOrderTraversal(withRootNode: child)
+        }
+    }
+
+    public func postOrderTraversal(withRootNode node: NAryTreeNode<T>?) {
+        guard let node = node else {
+            return
+        }
+        for child in node.children {
+            preOrderTraversal(withRootNode: child)
+        }
+        print("\(node.data)", terminator: " --> ")
+    }
+
+}
